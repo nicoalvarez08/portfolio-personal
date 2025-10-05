7634c3858c5cb7b9696039f764efb1072c77a216
nicolas alvarez <nicoalvlu@gmail.com>
2025-10-04 23:19:18 -0300

    Disable TypeScript unused variable errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-personal/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  }
 })
